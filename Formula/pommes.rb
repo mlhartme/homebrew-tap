7fd2a548cdc9fa4fa984d232e863d30292198361
Michael Hartmeier <mlhartme.users.noreply@github.com>
2023-10-20 22:05:49 +0200

    remove head option

--- Formula/pommes.rb
+++ Formula/pommes.rb
@@ -3,7 +3,6 @@ class Pommes < Formula
   homepage "https://github.com/mlhartme/pommes"
   url "https://github.com/mlhartme/pommes/archive/refs/tags/pommes-3.4.0.tar.gz"
   sha256 "d7fadf5538b0ed79bec622ef98875e7637aa6a63c0cb110d84fb3b2cde13025b"
-  head "https://github.com/mlhartme/pommes.git"
   license "Apache-2.0"
 
   depends_on "maven" => :build
