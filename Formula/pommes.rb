dd65a44ab1bf102636b2c307bcb1e8b6ab115c2f
Michael Hartmeier <mlhartme.users.noreply@github.com>
2023-10-21 08:31:55 +0200

    test and completion

--- Formula/pommes.rb
+++ Formula/pommes.rb
@@ -3,16 +3,17 @@ class Pommes < Formula
   homepage "https://github.com/mlhartme/pommes"
   url "https://github.com/mlhartme/pommes/archive/refs/tags/pommes-3.4.0.tar.gz"
   sha256 "d7fadf5538b0ed79bec622ef98875e7637aa6a63c0cb110d84fb3b2cde13025b"
-  license "Apache-2.0"
+      license "Apache-2.0"
 
   depends_on "maven" => :build
 
   def install
     system "mvn -U clean package"
     bin.install "target/pommes"
+    zsh_completion.install "pom.xml" => "_pommes"
   end
 
   test do
-    system "false"
+    assert_match "USAGE:", shell_output("#{bin}/pommes -h 2>&1")
   end
 end
